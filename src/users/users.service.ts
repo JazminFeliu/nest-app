bb6cb91cfe5d92324d8ce9ea476c4b7e4ba5315e
JazminFeliu <jazminfeliu@gmail.com>
2022-04-03 15:41:20 -0300

    subo conf openApi + mongoose + mongocli

---
+++ src/users/users.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from './user.model';
+import { Model } from 'mongoose';
+
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  create(user:User): Promise<User> {
+    return this.userModel.create(user);
+    
+  }
+
+  findAll() {
+    console.log(this.userModel.find());
+    return this.userModel.find();
+  }
+
+  findOne(id: string)  {
+    return this.userModel.findById(id);
+  }
+
+  update(id: string, user: User) {
+    return this.userModel.findByIdAndUpdate(id, user);
+  }
+
+  remove(id: string) {
+    return this.userModel.findByIdAndDelete(id);
+  }
+}
